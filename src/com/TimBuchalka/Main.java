65385428149d309d648463b9ae5945b9da97ac20
DSkilton <the_skiltons@hotmail.com>
2019-01-12 16:05:56 +0000

    Initial commit

--- src/com/TimBuchalka/Main.java
+++ src/com/TimBuchalka/Main.java
@@ -22,6 +22,46 @@ class Jaws extends Movie{
     }
 }
 
+class IndependanceDay extends Movie {
+    public IndependanceDay(){
+        super("Independance Day");
+    }
+
+    @Override
+    public String plot(){
+        return "Aliens attempt to take over earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner(){
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot(){
+        return "Kids try and escape a maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars(){
+        super ("Star Wars");
+    }
+
+    @Override
+    public String plot(){
+        return "Imperial Forces battle for control of the universe";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable(){
+        super("Forgetable");
+    }
+
+    // no plot
+}
 public class Main {
 
     public static void main(String[] args) {
